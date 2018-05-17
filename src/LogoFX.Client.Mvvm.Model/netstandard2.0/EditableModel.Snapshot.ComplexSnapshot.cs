e9ec1327433f861861af905e7c719631cab48235
dmitrybublik <dmitry.buba@gmail.com>
2018-05-17 11:50:10 +0300

    Revert "Fixed properties notification call place."
    This reverts commit 18577c14b0d89c06f1de0a413e5a9f09bd2dc289.

--- src/LogoFX.Client.Mvvm.Model/netstandard2.0/EditableModel.Snapshot.ComplexSnapshot.cs
+++ src/LogoFX.Client.Mvvm.Model/netstandard2.0/EditableModel.Snapshot.ComplexSnapshot.cs
@@ -213,6 +213,11 @@ namespace LogoFX.Client.Mvvm.Model
                             _fieldSnapshots.Add(fieldInfo, snapshot);
                         }
                     }
+
+                    if (model is INotifyPropertyChanged)
+                    {
+                        NotifyAllPropertiesChanged((INotifyPropertyChanged) model);
+                    }
                 }
 
                 private bool CheckBaseType(Type type, Type baseType)
@@ -318,11 +323,6 @@ namespace LogoFX.Client.Mvvm.Model
                             var value = snapshot.GetValue(cache);
                             memberInfo.SetValue(model, value);
                         }
-                    }
-
-                    if (model is INotifyPropertyChanged)
-                    {
-                        NotifyAllPropertiesChanged((INotifyPropertyChanged) model);
                     }
 
                     if (model is EditableModel<T> editableModel)
