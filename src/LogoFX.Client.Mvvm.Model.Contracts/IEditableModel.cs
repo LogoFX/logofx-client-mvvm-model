78876f73ea23d9409ba84675e0444f5c0e33686f
Gennady Verdel <godrose@gmail.com>
2018-11-13 10:28:18 +0200

    Dropped .NETStandard20 suffix from Model.Contracts

---
+++ src/LogoFX.Client.Mvvm.Model.Contracts/IEditableModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace LogoFX.Client.Mvvm.Model.Contracts
+{
+    /// <summary>
+    /// Represents editable model
+    /// </summary>
+    public interface IEditableModel :
+        IEditableObject,
+        ICanBeDirty, 
+        ICanCancelChanges, 
+        ICanCommitChanges
+    {
+
+    }
+}
---
+++ src/LogoFX.Client.Mvvm.Model.Contracts/IFilterModel.cs
@@ -0,0 +1,17 @@
+﻿#if NETSTANDARD2_0
+using System;
+#endif
+
+namespace LogoFX.Client.Mvvm.Model.Contracts
+{
+    /// <summary>
+    /// Represents filter model
+    /// </summary>
+    public interface IFilterModel : IValueObject
+#if NETSTANDARD2_0
+          , ICloneable
+#endif      
+    {
+
+    }
+}
