bcf13af8a447f46bff8dc0b55ddfd32ba3471cce
godrose <godrose@gmail.com>
2019-04-18 16:47:26 +0300

    fix: handling the case for properties without validation attributes closes #13

---
+++ src/LogoFX.Client.Mvvm.Model.Tests/SimpleEditableModelErrorsTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace LogoFX.Client.Mvvm.Model.Tests
+{
+    public class SimpleEditableModelErrorsTests
+    {
+        [Fact]        
+        public void GetErrors_PropertyDoesnHaveValidationInfo_ErrorsAreEmpty()
+        {
+            var model = new SimpleEditableModel("Test", 21);
+
+            var errors = model.GetErrors("Age");
+
+            errors.Should().BeEmpty();
+        }
+    }
+}
