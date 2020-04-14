0e244b4c01c157ac3eded26ff801a0b6383bc77c
Gennady Verdel <godrose@gmail.com>
2020-04-14 10:45:05 +0300

    feat(bdd): added composite model validation scenarios

---
+++ src/LogoFX.Client.Mvvm.Model.Tests/CompositeEditableModelValidationSteps.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace LogoFX.Client.Mvvm.Model.Tests
+{
+    [Binding]
+    internal sealed class CompositeEditableModelValidationSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public CompositeEditableModelValidationSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [When(@"The composite editable model is created")]
+        public void WhenTheCompositeEditableModelIsCreated()
+        {
+            var compositeModel = new CompositeEditableModel("location");
+            var isRaised = false;
+            var isRaisedRef= new WeakReference(isRaised);
+            compositeModel.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == "Error")
+                {
+                    isRaisedRef.Target = true;
+                }
+            };
+            _scenarioContext.Add("compositeModel", compositeModel);
+            _scenarioContext.Add("isRaisedRef", isRaisedRef);
+        }
+
+        [When(@"The internal model property is assigned a valid value")]
+        public void WhenTheInternalModelPropertyIsAssignedAValidValue()
+        {
+            var compositeModel = _scenarioContext.Get<CompositeEditableModel>("compositeModel");
+            compositeModel.Person.Name = DataGenerator.ValidName;
+        }
+
+        [When(@"The internal model property is assigned an invalid value")]
+        public void WhenTheInternalModelPropertyIsAssignedAnInvalidValue()
+        {
+            var compositeModel = _scenarioContext.Get<CompositeEditableModel>("compositeModel");
+            compositeModel.Person.Name = DataGenerator.InvalidName;
+        }
+
+        [When(@"The internal model is reset")]
+        public void WhenTheInternalModelIsReset()
+        {
+            var compositeModel = _scenarioContext.Get<CompositeEditableModel>("compositeModel");
+            compositeModel.Person = new SimpleEditableModel(DataGenerator.ValidName, 0);
+        }
+
+        [Then(@"The composite editable model has no errors")]
+        public void ThenTheCompositeEditableModelHasNoErrors()
+        {
+            var compositeModel = _scenarioContext.Get<CompositeEditableModel>("compositeModel");
+            AssertHelper.AssertModelHasErrorIsFalse(compositeModel);
+        }
+
+        [Then(@"The composite editable model has errors")]
+        public void ThenTheCompositeEditableModelHasErrors()
+        {
+            var compositeModel = _scenarioContext.Get<CompositeEditableModel>("compositeModel");
+            AssertHelper.AssertModelHasErrorIsTrue(compositeModel);
+        }
+
+        [Then(@"The error notification should be raised")]
+        public void ThenTheErrorNotificationShouldBeRaised()
+        {
+            var isRaisedRef = _scenarioContext.Get<WeakReference>("isRaisedRef");
+            ((bool) isRaisedRef.Target).Should().BeTrue();
+        }
+    }
+}
---
+++ src/LogoFX.Client.Mvvm.Model.Tests/Validation.feature
@@ -0,0 +1,34 @@
+﻿Feature: Validation
+	In order to support various client app scenarios
+	As an app developer
+	I want to get notifications about model validity
+
+Scenario: Valid test value object should have no errors
+	When The simple test value object is created with name 'valid name'
+	Then The simple test value object has no errors
+
+Scenario: Invalid test value object should have errors
+	When The simple test value object is created with name 'in$valid name'
+	Then The simple test value object has errors
+
+Scenario: Assigning internal property a valid value should result in model with no errors
+	When The composite editable model is created
+	And The internal model property is assigned a valid value
+	Then The composite editable model has no errors
+
+Scenario: Assigning internal property an invalid value should result in model with errors
+	When The composite editable model is created
+	And The internal model property is assigned an invalid value
+	Then The composite editable model has errors
+
+Scenario: Resetting internal model should raise error notification
+	When The composite editable model is created
+	And The internal model is reset
+	Then The error notification should be raised
+
+#This feature isn't supported yet
+@Ignore
+Scenario: Assigning internal property an invalid value should raise error notification
+	When The composite editable model is created
+	And The internal model property is assigned an invalid value
+	Then The error notification should be raised
