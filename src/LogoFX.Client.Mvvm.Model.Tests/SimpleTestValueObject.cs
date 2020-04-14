d281dfbc6b0e9a13f540b2b81086ee17e79419ab
Gennady Verdel <godrose@gmail.com>
2020-04-14 10:14:48 +0300

    feat(bdd): restored missing test object

---
+++ src/LogoFX.Client.Mvvm.Model.Tests/SimpleTestValueObject.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LogoFX.Client.Mvvm.Model.Tests
+{
+    class SimpleTestValueObject : ValueObject
+    {
+        public SimpleTestValueObject(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        [NameValidation]
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class NameValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var str = value as string;
+            var isValid = str != null && str.Contains("$") == false;
+            if (isValid)
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Name is invalid");
+            }
+        }
+    }
+}
