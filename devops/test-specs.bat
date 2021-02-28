5af5770f9d105f19b538a4ce2e56b60b3d30a8c2
Gennady Verdel <godrose@gmail.com>
2021-02-28 16:07:18 +0200

    devops: fixed incorrect work item link #21

--- devops/test-specs.bat
+++ devops/test-specs.bat
@@ -1,5 +1,5 @@
 cd ..\src\LogoFX.Client.Mvvm.Model.Specs
 dotnet test
 cd bin
-livingdoc test-assembly LogoFX.Client.Mvvm.Model.Specs.dll -t TestExecution.json --work-item-url-template 'https://github.com/LogoFX/logofx-client-mvvm-model/issues/{id}' --work-item-prefix WI
+livingdoc test-assembly LogoFX.Client.Mvvm.Model.Specs.dll -t TestExecution.json --work-item-url-template https://github.com/LogoFX/logofx-client-mvvm-model/issues/{id} --work-item-prefix WI
 cd ..\..\..\devops
