c07ff6e8aaee901b07ab4de0c87eed2a29c275dd
Gennady Verdel <godrose@gmail.com>
2021-06-27 19:57:12 +0300

    chore: merge from develop #21 (#25)

--- src/LogoFX.Client.Mvvm.Model/EditableModel.Base.cs
+++ src/LogoFX.Client.Mvvm.Model/EditableModel.Base.cs
@@ -4,38 +4,7 @@ using LogoFX.Client.Core;
 namespace LogoFX.Client.Mvvm.Model
 {
     public partial class EditableModel<T>
-    {
-        /*
-        /// <summary>
-        /// Compares the current and new values. If they are different, optionally marks the model as dirty, 
-        /// updates the respective field 
-        /// and fires the property change notification.        
-        /// </summary>
-        /// <typeparam name="TProperty">The type of the property.</typeparam>
-        /// <param name="currentValue">The current value field reference.</param>
-        /// <param name="newValue">The new value.</param>
-        /// <param name="name">The property name.</param>
-        /// <param name="markAsDirty">True, if the model should be marked as dirty, false otherwise. The default value is <c>true</c></param>
-        [Obsolete]
-        protected void SetPropertyOld<TProperty>(
-            ref TProperty currentValue, 
-            TProperty newValue, 
-            [CallerMemberName] string name = "",
-            bool markAsDirty = true)
-        {
-            if (Equals(currentValue, newValue))
-            {
-                return;
-            }
-            if (markAsDirty)
-            {
-                MakeDirty();
-            }            
-            currentValue = newValue;
-            NotifyOfPropertyChange(name);
-        }
-        */
-
+    {        
         /// <summary>
         /// Compares the current and new values. If they are different,
         /// invokes the functionality which is set in the <b>options</b> parameter, 
