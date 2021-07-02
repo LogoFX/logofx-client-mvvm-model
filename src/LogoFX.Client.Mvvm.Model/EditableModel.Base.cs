96c477c44d89082cc0adf145ba5aadbc1716a210
Gennady Verdel <godrose@gmail.com>
2021-07-02 17:52:31 +0300

    chore: code cleanup

--- src/LogoFX.Client.Mvvm.Model/EditableModel.Base.cs
+++ src/LogoFX.Client.Mvvm.Model/EditableModel.Base.cs
@@ -1,5 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
-using LogoFX.Client.Core;
 
 namespace LogoFX.Client.Mvvm.Model
 {
@@ -42,16 +41,5 @@ namespace LogoFX.Client.Mvvm.Model
             }
             base.SetProperty(ref currentValue, newValue, options, name);
         }
-    }
-
-    /// <summary>
-    /// The <see cref="EditableModel"/> set property options
-    /// </summary>
-    public class EditableSetPropertyOptions : SetPropertyOptions
-    {
-        /// <summary>
-        /// True, if the model should be marked as dirty, false otherwise. The default value is <c>true</c>
-        /// </summary>
-        public bool MarkAsDirty { get; set; } = true;
     }
 }
